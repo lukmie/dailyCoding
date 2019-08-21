41f0cbea38ab7dfd371f839172dcd1e41bbb73c9
lukmie <lmienicki@gmail.com>
2019-08-21 23:25:58 +0200

    exe4

---
+++ w3resource/Array/src/main/java/com/lukmie/exe4_avgValArray/Exercise4.java
@@ -0,0 +1,21 @@
+package com.lukmie.exe4_avgValArray;
+
+public class Exercise4 {
+
+    private static int[] intTab = {20, 30, 25, 35, -16, 60, -100};
+
+    public static void main(String[] args) {
+        System.out.println(avgValArray(intTab));
+    }
+
+    public static double avgValArray(int[] array) {
+        double sum = 0;
+        double avg = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        avg = sum/array.length;
+        return avg;
+    }
+
+}
