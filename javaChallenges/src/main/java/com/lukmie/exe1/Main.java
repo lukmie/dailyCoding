831cfde3b37e49b5d790b345636353a4d99c0c0e
lukmie <lmienicki@gmail.com>
2019-11-18 15:36:13 +0100

    showTenMostPopularNames method added

--- javaChallenges/src/main/java/com/lukmie/exe1/Main.java
+++ javaChallenges/src/main/java/com/lukmie/exe1/Main.java
@@ -3,19 +3,31 @@ package com.lukmie.exe1;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        System.out.println(countLines());
+        System.out.println(showTenMostPopularNames());
 //        countLines();
     }
 
-    public static List<PersonalData> countLines() throws IOException {
-        Files.lines(Paths.get("Popular_Baby_Names.csv"))
-                .map(p -> new PersonalData(p.split(",")[3], Long.parseLong(p.split(",")[4])))
+    public static Map<String, Integer> showTenMostPopularNames() throws IOException {
+        List<PersonalData> personList = Files.lines(Paths.get("Popular_Baby_Names.csv"))
+                .map(p -> new PersonalData(p.split(",")[3], Integer.parseInt(p.split(",")[4])))
                 .collect(Collectors.toList());
+
+        Map<String, Integer> unsorted = personList.stream()
+                .collect(Collectors.groupingBy(PersonalData::getName, Collectors.summingInt(PersonalData::getCount)));
+
+        return unsorted.entrySet().stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .limit(10)
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
+                        (oldValue, newValue) -> oldValue, LinkedHashMap::new));
 
     }
 }
--- javaChallenges/src/main/java/com/lukmie/exe1/PersonalData.java
+++ javaChallenges/src/main/java/com/lukmie/exe1/PersonalData.java
@@ -4,10 +4,26 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Objects;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 public class PersonalData {
     private String name;
-    private Long count;
+    private Integer count;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof PersonalData)) return false;
+        PersonalData that = (PersonalData) o;
+        return getName().equals(that.getName()) &&
+                getCount().equals(that.getCount());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getCount());
+    }
 }
