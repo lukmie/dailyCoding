59e5837f0a3f3e92251cda431d839c62cd529ba7
lukmie <lmienicki@gmail.com>
2019-11-18 15:14:15 +0100

    Lombok added. New class added.

--- javaChallenges/src/main/java/com/lukmie/exe1/Main.java
+++ javaChallenges/src/main/java/com/lukmie/exe1/Main.java
@@ -3,16 +3,19 @@ package com.lukmie.exe1;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         System.out.println(countLines());
+//        countLines();
     }
 
-    public static long countLines() throws IOException {
-        return Files.lines(Paths.get("Popular_Baby_Names.csv"))
-//                .flatMap(l -> Arrays.stream(l.split(",")))
-                .flatMap(l -> l.split(","))
-                .count();
+    public static List<PersonalData> countLines() throws IOException {
+        Files.lines(Paths.get("Popular_Baby_Names.csv"))
+                .map(p -> new PersonalData(p.split(",")[3], Long.parseLong(p.split(",")[4])))
+                .collect(Collectors.toList());
+
     }
 }
---
+++ javaChallenges/src/main/java/com/lukmie/exe1/PersonalData.java
@@ -0,0 +1,13 @@
+package com.lukmie.exe1;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class PersonalData {
+    private String name;
+    private Long count;
+}
